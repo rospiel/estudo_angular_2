3a60c295b9996922243d5d313539e0c19cea0efd
Rodrigo <rospielberg@gmail.com>
2019-02-17 12:49:11 -0300

    Adicionando bootstrap ao projeto. Criando os components de contato, nossa visao e quem somos. Criando o menu de acesso aos novos componentes com bootstrap.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { QuemSomosComponent } from './quem-somos/quem-somos.component';
+import { ContatoComponent } from './contato/contato.component';
+import { NossaVisaoComponent } from './nossa-visao/nossa-visao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    QuemSomosComponent,
+    ContatoComponent,
+    NossaVisaoComponent
   ],
   imports: [
     BrowserModule
